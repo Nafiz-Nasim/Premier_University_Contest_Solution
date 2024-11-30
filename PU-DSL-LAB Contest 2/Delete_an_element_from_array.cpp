231bf81a0ca08ea91cb910624c4f602c3be9bb07
A. K. M. Nafiz Ibn Nasim <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-11-30 20:03:29 +0600

    Update Delete_an_element_from_array.cpp

--- PU-DSL-LAB Contest 2/Delete_an_element_from_array.cpp
+++ PU-DSL-LAB Contest 2/Delete_an_element_from_array.cpp
@@ -9,7 +9,7 @@ int main()
     int position;
     cin>>position;
 
-    for(int i=0;i<size;i++)
+    for(int i=0;i<size-1;i++)
     {
         
         if(i+1<position) 
@@ -22,7 +22,7 @@ int main()
             
             
         
-            cout<<array[i]<<" ";
+            cout<<array[i+1]<<" ";
             
         }
         
