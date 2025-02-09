51422f57e6253c4abcf088a868fbd7a6a5a25a8f
A. K. M. Nafiz Ibn Nasim <akm.nafiz.ibn.nasim.bd@gmail.com>
2025-02-09 16:26:18 +0600

    Update R_n_th_prime.cpp

--- Online Contest 02(batch 45 cp)/R_n_th_prime.cpp
+++ Online Contest 02(batch 45 cp)/R_n_th_prime.cpp
@@ -1,37 +1,29 @@
-/////////////Wrong ans in t77///////////////////// not accepted/////////
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    const int max_n = 10000;           // Maximum limit------ prime numbers
-    vector<int> pList;                 //  prime number rakhar list
-    vector<bool> isP(max_n + 1, true); // shob 1
+int main() {
+    const int max_n = 110000; // Maximum limit------ prime numbers
+    vector<int> pList;    //  prime number rakhar list
+    vector<int> isP(max_n + 1, 1); //shob 1
 
     // gunitok a tar oi number ta rakbo //sir er cp class er prime ber korar niom
-    for (int i = 2; i * i <= max_n; i++)
-    {
-        if (isP[i])
-        {
-            for (int j = i * i; j <= max_n; j += i)
-            {
-                isP[j] = false;
+    for (int i = 2; i*i  <= max_n; i++) {
+        if (isP[i]) {
+            for (int j = i * i; j <= max_n; j += i) {
+                isP[j] = 0;
             }
         }
     }
 
     // prime gula k alada kore
-    for (int i = 2; i <= max_n; i++)
-    {
-        if (isP[i])
-            pList.push_back(i);
+    for (int i = 2; i <= max_n; i++) {
+        if (isP[i]) pList.push_back(i);
     }
 
     int t, n;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         cin >> n;
         cout << pList[n - 1] << endl;
     }
